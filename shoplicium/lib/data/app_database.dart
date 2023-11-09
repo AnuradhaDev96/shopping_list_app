ade4264d6fac661c7ec68bd3775da31fac3b75f1
AnuradhaDev96 <anusampath9470@gmail.com>
2023-11-09 20:00:43 +0530

    Implementation - Sqflite setup

--- shoplicium/lib/data/app_database.dart
+++ shoplicium/lib/data/app_database.dart
@@ -23,9 +23,11 @@ abstract class AppDatabase {
 
 abstract class DbHelper {
   static const dbName = 'shoplicium_database.db';
+  static const shoppingListsTable = 'ShoppingLists';
+  static const listItemsTable = 'ListItems';
 
   static const createListTableCommand =
-      'CREATE TABLE ShoppingLists(listId TEXT PRIMARY KEY, title TEXT, date TEXT, createdOn TEXT)';
+      'CREATE TABLE $shoppingListsTable(listId TEXT PRIMARY KEY, title TEXT, date TEXT, createdOn TEXT)';
   static const createListItemsTableCommand =
-      'CREATE TABLE ListItems(itemId TEXT PRIMARY KEY, title TEXT, amount INTEGER, UOM INTEGER, status INTEGER, listId TEXT, FOREIGN KEY(listId) REFERENCES ShoppingLists(listId))';
+      'CREATE TABLE $listItemsTable(itemId TEXT PRIMARY KEY, title TEXT, amount INTEGER, UOM INTEGER, status INTEGER, listId TEXT, FOREIGN KEY(listId) REFERENCES ShoppingLists(listId))';
 }
