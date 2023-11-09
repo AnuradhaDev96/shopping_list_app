83c9f2205d510eb1c82cfa950b5f55dc8fc4bb23
AnuradhaDev96 <anusampath9470@gmail.com>
2023-11-09 14:13:34 +0530

    Implementation - Create new shopping list

--- shoplicium/lib/presentation/views/shopping_list/widgets/create_new_list_dialog.dart
+++ shoplicium/lib/presentation/views/shopping_list/widgets/create_new_list_dialog.dart
@@ -1,15 +1,26 @@
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+import 'package:table_calendar/table_calendar.dart';
 
 import '../../../../config/themes/input_decorations.dart';
 import '../../../widgets/primary_button_skin.dart';
 
-class CreateNewListDialog extends StatelessWidget {
+class CreateNewListDialog extends StatefulWidget {
   CreateNewListDialog({super.key});
 
+  @override
+  State<CreateNewListDialog> createState() => _CreateNewListDialogState();
+}
+
+class _CreateNewListDialogState extends State<CreateNewListDialog> {
   final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
 
   final _titleController = TextEditingController();
   final _dateController = TextEditingController();
+
+  DateTime _selectedDay = DateTime.now();
+  DateTime _focusedDay = DateTime.now();
+  CalendarFormat _calendarFormat = CalendarFormat.month;
 
   @override
   Widget build(BuildContext context) {
@@ -58,7 +69,6 @@ class CreateNewListDialog extends StatelessWidget {
                     },
                     decoration: InputDecorations.outlinedInputDecoration(hintText: 'ex: Awesome shopping'),
                   ),
-
                   const SizedBox(height: 22),
                   const Align(
                     alignment: Alignment.centerLeft,
@@ -73,6 +83,8 @@ class CreateNewListDialog extends StatelessWidget {
                   ),
                   TextFormField(
                     controller: _dateController,
+                    readOnly: true,
+                    onTap: () => showDatePickerDialog(context),
                     validator: (value) {
                       if (value == null || value.isEmpty) {
                         return 'Date is required';
@@ -94,6 +106,43 @@ class CreateNewListDialog extends StatelessWidget {
           ),
         ],
       ),
+    );
+  }
+
+  void showDatePickerDialog(BuildContext context) {
+    showDialog(
+      context: context,
+      builder: (pickerContext) {
+        return Dialog(
+          child: Wrap(
+            children: [
+              TableCalendar(
+                focusedDay: _focusedDay,
+                calendarFormat: _calendarFormat,
+                firstDay: DateTime.now(),
+                lastDay: DateTime.now().add(
+                  const Duration(days: 365 * 10),
+                ),
+                selectedDayPredicate: (day) {
+                  return isSameDay(_selectedDay, day);
+                },
+                onDaySelected: (selectedDay, focusedDay) {
+                  setState(() {
+                    _selectedDay = selectedDay;
+                    _focusedDay = focusedDay;
+
+                    _dateController.text = DateFormat("EEEE d, MMM y").format(_selectedDay);
+                  });
+                  Navigator.pop(context);
+                },
+                onFormatChanged: (format) {
+                  return;
+                },
+              )
+            ],
+          ),
+        );
+      },
     );
   }
 }
