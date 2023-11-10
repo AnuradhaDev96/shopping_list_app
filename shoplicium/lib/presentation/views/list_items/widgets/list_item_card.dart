e22a6e65f1c17ba6d4fc7c1a3c15965f42ea594a
AnuradhaDev96 <anusampath9470@gmail.com>
2023-11-10 17:47:11 +0530

    Implementation - List item change status

--- shoplicium/lib/presentation/views/list_items/widgets/list_item_card.dart
+++ shoplicium/lib/presentation/views/list_items/widgets/list_item_card.dart
@@ -1,15 +1,21 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_svg/svg.dart';
 
 import '../../../../domain/enums/list_item_status_enum.dart';
 import '../../../../domain/models/list_item_dto.dart';
 import '../../../../utils/constants/app_colors.dart';
 import '../../../../utils/constants/assets.dart';
+import '../../../../utils/resources/message_utils.dart';
+import '../../../cubits/shopping_items/update_item_status_cubit.dart';
+import '../../../states/data_payload_state.dart';
 
 class ListItemCard extends StatelessWidget {
-  const ListItemCard({super.key, required this.data});
+  ListItemCard({super.key, required this.data});
 
   final ListItemDto data;
+
+  final _updateStatusCubit = UpdateItemStatusCubit();
 
   @override
   Widget build(BuildContext context) {
@@ -27,46 +33,56 @@ class ListItemCard extends StatelessWidget {
           ),
         ],
       ),
-      child: Row(
-        crossAxisAlignment: CrossAxisAlignment.end,
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        children: [
-          Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
+      child: BlocProvider<UpdateItemStatusCubit>(
+        create: (context) => _updateStatusCubit,
+        child: BlocListener<UpdateItemStatusCubit, DataPayloadState>(
+          listener: (context, state) {
+            if (state is ErrorState) {
+              MessageUtils.showSnackBarOverBarrier(context, state.errorMessage, isErrorMessage: true);
+            }
+          },
+          child: Row(
+            crossAxisAlignment: CrossAxisAlignment.end,
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
             children: [
-              Text(
-                data.title,
-                style: const TextStyle(
-                  color: Colors.black,
-                  fontWeight: FontWeight.bold,
-                  fontSize: 17,
-                ),
-              ),
-              const SizedBox(height: 2),
-              Text(
-                '${data.amount} ${data.UOM.symbol}',
-                style: const TextStyle(
-                  color: Colors.black,
-                  fontWeight: FontWeight.normal,
-                  fontSize: 14,
-                ),
-              ),
-              if (data.status != ListItemStatusEnum.remaining)
-                Padding(
-                  padding: const EdgeInsets.only(top: 16),
-                  child: GestureDetector(
-                    behavior: HitTestBehavior.opaque,
-                    onTap: () {},
-                    child: const Text(
-                      "Reset",
-                      style: TextStyle(color: AppColors.darkBlue1, fontSize: 18, fontWeight: FontWeight.w600),
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    data.title,
+                    style: const TextStyle(
+                      color: Colors.black,
+                      fontWeight: FontWeight.bold,
+                      fontSize: 17,
                     ),
                   ),
-                ),
+                  const SizedBox(height: 2),
+                  Text(
+                    '${data.amount} ${data.UOM.symbol}',
+                    style: const TextStyle(
+                      color: Colors.black,
+                      fontWeight: FontWeight.normal,
+                      fontSize: 14,
+                    ),
+                  ),
+                  if (data.status != ListItemStatusEnum.remaining)
+                    Padding(
+                      padding: const EdgeInsets.only(top: 16),
+                      child: GestureDetector(
+                        behavior: HitTestBehavior.opaque,
+                        onTap: () => changeItemStatus(ListItemStatusEnum.remaining),
+                        child: const Text(
+                          "Reset",
+                          style: TextStyle(color: AppColors.darkBlue1, fontSize: 18, fontWeight: FontWeight.w600),
+                        ),
+                      ),
+                    ),
+                ],
+              ),
+              _manageStatusWidget(),
             ],
           ),
-          _manageStatusWidget(),
-        ],
+        ),
       ),
     );
   }
@@ -79,9 +95,15 @@ class ListItemCard extends StatelessWidget {
             Row(
               mainAxisAlignment: MainAxisAlignment.end,
               children: [
-                SvgPicture.asset(Assets.itemNotInShopIcon, width: 32, height: 32),
+                GestureDetector(
+                  onTap: () => changeItemStatus(ListItemStatusEnum.notInShop),
+                  child: SvgPicture.asset(Assets.itemNotInShopIcon, width: 32, height: 32),
+                ),
                 const SizedBox(width: 6),
-                SvgPicture.asset(Assets.itemAddToBagIcon, width: 32, height: 32),
+                GestureDetector(
+                  onTap: () => changeItemStatus(ListItemStatusEnum.inBag),
+                  child: SvgPicture.asset(Assets.itemAddToBagIcon, width: 32, height: 32),
+                ),
               ],
             ),
           ],
@@ -92,4 +114,6 @@ class ListItemCard extends StatelessWidget {
         return SvgPicture.asset(Assets.itemMarkedAsInShopIcon);
     }
   }
+
+  void changeItemStatus(ListItemStatusEnum newStatus) => _updateStatusCubit.updateStatus(newStatus, data);
 }
