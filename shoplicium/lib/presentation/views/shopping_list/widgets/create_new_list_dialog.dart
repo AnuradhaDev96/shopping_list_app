69d8b21fa18e1b4691f4d64d3aea679189671af4
AnuradhaDev96 <anusampath9470@gmail.com>
2023-11-13 22:50:39 +0530

    Resolve bugs

--- shoplicium/lib/presentation/views/shopping_list/widgets/create_new_list_dialog.dart
+++ shoplicium/lib/presentation/views/shopping_list/widgets/create_new_list_dialog.dart
@@ -35,114 +35,120 @@ class _CreateNewListDialogState extends State<CreateNewListDialog> {
       shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(16)),
       backgroundColor: Colors.white,
       surfaceTintColor: Colors.transparent,
-      child: Wrap(
-        children: [
-          Padding(
-            padding: const EdgeInsets.only(top: 26, bottom: 31, left: 20, right: 20),
-            child: Form(
-              key: _formKey,
-              child: Column(
-                children: [
-                  const Padding(
-                    padding: EdgeInsets.only(bottom: 23),
-                    child: Text(
-                      'Create new list',
-                      style: TextStyle(
-                        color: Colors.black,
-                        fontSize: 18,
-                        fontWeight: FontWeight.w600,
+      child: FractionallySizedBox(
+        heightFactor: 0.67,
+        child: Padding(
+          // heightFactor: 0.8,
+          padding: const EdgeInsets.only(top: 26, bottom: 31, left: 20, right: 20),
+          child: CustomScrollView(
+            slivers: [
+              SliverToBoxAdapter(
+                child: Form(
+                  key: _formKey,
+                  child: Column(
+                    children: [
+                      const Padding(
+                        padding: EdgeInsets.only(bottom: 23),
+                        child: Text(
+                          'Create new list',
+                          style: TextStyle(
+                            color: Colors.black,
+                            fontSize: 18,
+                            fontWeight: FontWeight.w600,
+                          ),
+                        ),
                       ),
-                    ),
-                  ),
-                  const Align(
-                    alignment: Alignment.centerLeft,
-                    child: Text(
-                      'Title',
-                      style: TextStyle(
-                        color: Colors.black,
-                        fontSize: 18,
-                        fontWeight: FontWeight.w500,
+                      const Align(
+                        alignment: Alignment.centerLeft,
+                        child: Text(
+                          'Title',
+                          style: TextStyle(
+                            color: Colors.black,
+                            fontSize: 18,
+                            fontWeight: FontWeight.w500,
+                          ),
+                        ),
                       ),
-                    ),
-                  ),
-                  TextFormField(
-                    autovalidateMode: AutovalidateMode.onUserInteraction,
-                    controller: _titleController,
-                    validator: (value) {
-                      if (value == null || value.isEmpty) {
-                        return 'Title is required';
-                      }
-
-                      return null;
-                    },
-                    style: TextStyles.textFieldInputTextStyle,
-                    decoration: InputDecorations.outlinedInputDecoration(hintText: 'ex: Awesome shopping'),
-                  ),
-                  const SizedBox(height: 22),
-                  const Align(
-                    alignment: Alignment.centerLeft,
-                    child: Text(
-                      'Shopping date',
-                      style: TextStyle(
-                        color: Colors.black,
-                        fontSize: 18,
-                        fontWeight: FontWeight.w500,
-                      ),
-                    ),
-                  ),
-                  TextFormField(
-                    controller: _dateController,
-                    autovalidateMode: AutovalidateMode.onUserInteraction,
-                    readOnly: true,
-                    onTap: () => showDatePickerDialog(context),
-                    validator: (value) {
-                      if (value == null || value.isEmpty) {
-                        return 'Date is required';
-                      }
-
-                      return null;
-                    },
-                    style: TextStyles.textFieldInputTextStyle,
-                    decoration: InputDecorations.outlinedInputDecoration(hintText: 'Tap to select date'),
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.only(top: 120),
-                    child: BlocProvider<CreateNewListCubit>(
-                      create: (context) => _createNewListCubit,
-                      child: BlocListener(
-                        bloc: _createNewListCubit,
-                        listener: (context, state) {
-                          if (state is ErrorState) {
-                            MessageUtils.showSnackBarOverBarrier(context, state.errorMessage, isErrorMessage: true);
-                          } else if (state is SuccessState) {
-                            Navigator.pop(context);
-                            MessageUtils.showSnackBarOverBarrier(context, 'List saved successfully');
+                      TextFormField(
+                        autovalidateMode: AutovalidateMode.onUserInteraction,
+                        controller: _titleController,
+                        validator: (value) {
+                          if (value == null || value.isEmpty) {
+                            return 'Title is required';
                           }
-                        },
-                        child: BlocBuilder<CreateNewListCubit, DataPayloadState>(
-                            bloc: _createNewListCubit,
-                            builder: (context, state) {
-                              if (state is RequestingState) {
-                                return const CupertinoActivityIndicator();
-                              }
 
-                              return GestureDetector(
-                                onTap: () {
-                                  saveListAndAddItems();
-                                },
-                                child: const PrimaryButtonSkin(
-                                  title: 'Save & add items',
-                                ),
-                              );
-                            }),
+                          return null;
+                        },
+                        style: TextStyles.textFieldInputTextStyle,
+                        decoration: InputDecorations.outlinedInputDecoration(hintText: 'ex: Awesome shopping'),
                       ),
-                    ),
-                  )
-                ],
+                      const SizedBox(height: 22),
+                      const Align(
+                        alignment: Alignment.centerLeft,
+                        child: Text(
+                          'Shopping date',
+                          style: TextStyle(
+                            color: Colors.black,
+                            fontSize: 18,
+                            fontWeight: FontWeight.w500,
+                          ),
+                        ),
+                      ),
+                      TextFormField(
+                        controller: _dateController,
+                        autovalidateMode: AutovalidateMode.onUserInteraction,
+                        readOnly: true,
+                        onTap: () => showDatePickerDialog(context),
+                        validator: (value) {
+                          if (value == null || value.isEmpty) {
+                            return 'Date is required';
+                          }
+
+                          return null;
+                        },
+                        style: TextStyles.textFieldInputTextStyle,
+                        decoration: InputDecorations.outlinedInputDecoration(hintText: 'Tap to select date'),
+                      ),
+                      Padding(
+                        padding: EdgeInsets.only(top: MediaQuery.sizeOf(context).height * 0.075),
+                        child: BlocProvider<CreateNewListCubit>(
+                          create: (context) => _createNewListCubit,
+                          child: BlocListener(
+                            bloc: _createNewListCubit,
+                            listener: (context, state) {
+                              if (state is ErrorState) {
+                                MessageUtils.showSnackBarOverBarrier(context, state.errorMessage, isErrorMessage: true);
+                              } else if (state is SuccessState) {
+                                Navigator.pop(context);
+                                MessageUtils.showSnackBarOverBarrier(context, 'List saved successfully');
+                              }
+                            },
+                            child: BlocBuilder<CreateNewListCubit, DataPayloadState>(
+                                bloc: _createNewListCubit,
+                                builder: (context, state) {
+                                  if (state is RequestingState) {
+                                    return const CupertinoActivityIndicator();
+                                  }
+
+                                  return GestureDetector(
+                                    onTap: () {
+                                      saveListAndAddItems();
+                                    },
+                                    child: const PrimaryButtonSkin(
+                                      title: 'Save & add items',
+                                    ),
+                                  );
+                                }),
+                          ),
+                        ),
+                      )
+                    ],
+                  ),
+                ),
               ),
-            ),
+            ],
           ),
-        ],
+        ),
       ),
     );
   }
